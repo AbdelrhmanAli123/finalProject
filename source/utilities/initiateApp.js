1c11759f38cbbd0b399b79032c997de73d651646
diaa <diaaabdelazizelshafie4491@gmail.com>
2024-03-15 00:42:36 +0200

    3rd_14/3

--- source/utilities/initiateApp.js
+++ source/utilities/initiateApp.js
@@ -50,7 +50,7 @@ const initiateApp = (app, express) => {
     })
     let clients = {}
     const io = initiateIo(server)
-    io.on('connection', async (socket) => {
+    io.on('connect', async (socket) => {
         console.log({
             message: "socket connected!",
             socketId: socket.id,
